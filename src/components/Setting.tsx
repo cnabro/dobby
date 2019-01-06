ef80f8a60409110ff6a1b1c8806651a7382449cd
yeonkko <nnnnyk@gmail.com>
2019-01-06 11:09:36 +0900

    :sparkles: initial components for router

---
+++ src/components/Setting.tsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+
+class Setting extends PureComponent {
+  render() {
+    return (
+      <React.Fragment>
+          Setting
+      </React.Fragment>
+    );
+  }
+}
+
+export default Setting;
---
+++ src/components/Timeline.tsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+
+class Timeline extends PureComponent {
+  render() {
+    return (
+      <React.Fragment>
+          Timeline
+      </React.Fragment>
+    );
+  }
+}
+
+export default Timeline;
